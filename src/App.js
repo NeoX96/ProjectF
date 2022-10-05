5a264176896b935e105cd486ca5f0acb9d47d6af
Vale <valentin@wuerfelmail.de>
2022-10-05 22:10:08 +0200

    Added Home,Chat,Maps,Login Sites

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
+import Maps from "./pages/Maps";
+import Login from "./pages/Login";
 
-const App = () => {
-    return (
-        <h1>Project-F</h1>
-    );
+
+function App() {
+    
+  return (
+    <div className="App">
+    <Router>
+        <Routes>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Chat" element={<Chat />} />
+            <Route path="/Maps" element={<Maps />} />
+            <Route path="/Login" element={<Login />} />
+        </Routes>
+    </Router>
+
+    </div>
+  );
 }
 
 export default App;
