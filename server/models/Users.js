7b7212857812b2a5c6deaae6c4b323d82a45c3e2
Vale <valentin@wuerfelmail.de>
2022-10-09 23:46:41 +0200

    User Schema + Passwort verschlüsselung

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,22 +1,35 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
+// MongoDB User Tabelle
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    maxlength: 30
   },
   age: {
     type: Number,
-    required: true,
+    maxlength: 3
   },
   email: {
     type: String,
     required: true,
+    unique: true,
+    maxlength: 50
   },
-  passwordHash: {
+  password: {
     type: String,
     required: true,
+    minlength: 8
   }
+});
+
+// Passwort Verschlüsselung mit bycript
+UserSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 const UserModel = mongoose.model("users", UserSchema);
