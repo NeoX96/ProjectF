70408aea5601bf2a0d24420bc9c0961de3caa736
docnopain <schmid.xyz@gmail.com>
2022-10-12 12:45:36 +0200

    possible loadingscreen

---
+++ client/src/pages/wait.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+export default class SignUp extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Wait just one more second</h3>
+            </div>
+        );
+    }
+}
+
+/*
+import React, { Component } from "react";
+import {useState} from 'react';
+export default class SignUp extends Component {
+    render() {
+        return (
+            <div>const[value, setValue] = ustState(0);
+                <h3>React SignUp Component</h3>         
+                 <div className="d-grid gap-2 mt-3">
+              <button onClick={() =>setValue(value + 1)}>
+                Los !
+              </button>
+            </div>
+            </div>
+        );
+    }
+    */
