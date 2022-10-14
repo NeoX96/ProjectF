d5dff1ecb5da598932b67fcc9852d8ae3c0174e4
docnopain <schmid.xyz@gmail.com>
2022-10-14 18:50:29 +0200

    Einbindung PW forgot Site

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Container, Col , Row  } from "react-router-dom";
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
 import Maps from "./pages/Maps";
 import Login from "./pages/Login";
 import Settings from "./pages/settings";
 import Wait from "./pages/wait";
+import Forgotpw from "./pages/Forgotpw";
+
 
 
 function App() {
@@ -23,6 +25,7 @@ function App() {
             <Route path="/Login" element={<Login />} />
             <Route path="/Settings" element={<Settings />} />
             <Route path="/Wait" element={<Wait />} />
+            <Route path="/Forgotpw" element={<Forgotpw />} />
             
         </Routes>
     </Router>
