3f21189be3149e1d93600f1830b1580252b63492
Vale <valentin@wuerfelmail.de>
2022-10-16 00:44:50 +0200

    added Chatting

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,8 +1,7 @@
-
-import './css/Chat.css';
-import React, { useEffect, useState } from 'react';
+import "./css/Chat.css";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import socketIO from "socket.io-client"
+import socketIO from "socket.io-client";
 
 const socket = socketIO.connect("http://localhost:4001");
 
@@ -10,57 +9,85 @@ function Chat() {
   const navigate = useNavigate();
 
   const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
+
 
   const sendMessage = () => {
     socket.emit("send_message", { message });
   };
 
-
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      setMessageReceived(data.message);
-    })
+      var item = document.createElement("li");
+      item.textContent = data.message;
+      document.getElementById("messages").appendChild(item);
+    });
+  }, []);
 
-  },[])
-  
+  function handleSubmit(event) {
+    event.preventDefault();
 
-  const sendPing = () => {
-    socket.emit('ping');
+    sendMessage();
+    setMessage("");
   }
 
 
   return (
-    <div>
-      <div class="navigation">
-      <h1> Chat </h1>
-        <button
-          onClick={() => {navigate("/Home");}}>Home
-        </button>
+    <div class="container">
+      <div class="row ">
+        <div class="navigation col ">
+          <h1> Chat</h1>
+          <button
+            onClick={() => {
+              navigate("/Home");
+            }}
+          >
+            Home
+          </button>
 
-        <button
-          onClick={() => {navigate("/Maps");}}>Maps
-        </button>
-          
-        <button
-          onClick={() => {navigate("/Login");}}>Logout
-        </button>
-      </div>
-      
-      <div>
-        <button onClick={sendPing}>Send ping</button>
-      </div>
+          <button
+            onClick={() => {
+              navigate("/Maps");
+            }}
+          >
+            Maps
+          </button>
 
-      <div>
-        <h2> Display: </h2>
-        
-          <input placeholder='Message...'  onChange={(event) => {setMessage(event.target.value)}}/>
-          <button onClick={sendMessage}>Send Message</button>
-          <ul id="messages">{messageReceived}</ul>
-      </div>
+          <button
+            onClick={() => {
+              navigate("/Login");
+            }}
+          >
+            Logout
+          </button>
+        </div>
 
+        <div id="test" class="col">
+          <ul id="messages"></ul>
+          <form onSubmit={handleSubmit}>
+            <div class="row">
+              <div class="col">
+                <input
+                  placeholder="Message..."
+                  required
+                  value={message}
+                  onChange={(event) => {
+                    setMessage(event.target.value);
+                  }}
+                  class="rounded form-control"
+                  type="text"
+                />
+              </div>
+              <div class="col">
+                <button class="btn btn-outline-primary" type="submit">
+                  Send Message
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Chat
+export default Chat;
