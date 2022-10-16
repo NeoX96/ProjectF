bfb71786c2f6e7df3da64e0ea5e6a8281a5cea85
Vale <valentin@wuerfelmail.de>
2022-10-17 01:26:24 +0200

    added autoscroll to newest message + overflow-box

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,5 +1,5 @@
 import "./css/Chat.css";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import socketIO from "socket.io-client";
 
@@ -8,21 +8,30 @@ const socket = socketIO.connect("http://localhost:4001");
 function Chat() {
   const navigate = useNavigate();
 
-  const [message, setMessage] = useState("");
+  // AutoScroll to Bottom
+  const fieldRef = useRef(null);
 
+  const [message, setMessage] = useState("");
 
   const sendMessage = () => {
     socket.emit("send_message", { message });
   };
 
+  // SocketIO Chat Message Receive and Send to Chat Field
   useEffect(() => {
     socket.on("receive_message", (data) => {
       var item = document.createElement("li");
       item.textContent = data.message;
       document.getElementById("messages").appendChild(item);
+
+      // AutoScroll to Bottom
+      fieldRef.current.scrollIntoView({
+        behavior: "smooth",
+      });
     });
   }, []);
 
+  // Call Functions when pressing submit
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -30,6 +39,10 @@ function Chat() {
     setMessage("");
   }
 
+  socket.on("connect", function () {
+    var client_id = socket.io.engine.id;
+    document.getElementById("SocketID").innerHTML = "ID: " + client_id;
+  });
 
   return (
     <div class="container">
@@ -59,12 +72,23 @@ function Chat() {
           >
             Logout
           </button>
+
+          <div class="col">
+            <h3 class="m-3">Socket IDs of all Users</h3>
+            <ul id="users"></ul>
+          </div>
         </div>
 
-        <div id="test" class="col">
-          <ul id="messages"></ul>
+        <div id="chatContainer" class="col">
+          <div
+            id="text"
+            class="overflow-auto d-flex flex-column justify-content-between rounded"
+          >
+            <ul id="messages"></ul>
+            <div ref={fieldRef}></div>
+          </div>
           <form onSubmit={handleSubmit}>
-            <div class="row">
+            <div class="row mt-2 mb-2">
               <div class="col">
                 <input
                   placeholder="Message..."
@@ -82,6 +106,7 @@ function Chat() {
                   Send Message
                 </button>
               </div>
+              <div class="col" id="SocketID"></div>
             </div>
           </form>
         </div>
