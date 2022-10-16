14d5a5c834724fb5ea988e9074ed8ac55eecc7ba
Vale <valentin@wuerfelmail.de>
2022-10-17 01:26:34 +0200

    renamed file

--- client/src/pages/assets/index.js
+++ client/src/pages/assets/index.js
@@ -4,7 +4,7 @@ import fussball from "./2.jpg";
 import volleyball from "./3.jpg";
 import baskettball from "./img5.jpg";
 import tischtennis from "./img4.jpg";
-import skateboard from "./6.jpg";
+import skateboard from "./img6.jpg";
 
 const images = {
     frisbee,
