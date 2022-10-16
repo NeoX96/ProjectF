bf2aaa5c6dac6c6a504fabfcfcdc10901e3c4177
docnopain <schmid.xyz@gmail.com>
2022-10-16 15:19:23 +0200

    added pictutres

--- client/src/pages/assets/index.js
+++ client/src/pages/assets/index.js
@@ -2,9 +2,9 @@
 import frisbee from "./1.jpg";
 import fussball from "./2.jpg";
 import volleyball from "./3.jpg";
-import baskettball from "./1gif.gif";
+import baskettball from "./img5.jpg";
 import tischtennis from "./img4.jpg";
-import skateboard from "./img5.jpg";
+import skateboard from "./6.jpg";
 
 const images = {
     frisbee,
