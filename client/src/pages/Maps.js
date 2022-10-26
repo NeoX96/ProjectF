93e3bbcac4298b513d6caa8faf6d4312302549ff
Valentin <valentin@wuerfelmail.de>
2022-10-26 13:30:57 +0200

    added Navbar as Object

--- client/src/pages/Maps.js
+++ client/src/pages/Maps.js
@@ -1,30 +1,16 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import './css/Chat.css';
 // Leaflet-React Import
 import { Marker, Popup, TileLayer, MapContainer} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-
+import Navigationbar from "./navigationbar";
 
 function Maps() {
-  const navigate = useNavigate();
-
   return (
     <div>
-      <div>
-        <h1> Maps </h1>
-        <button
-            onClick={() => {navigate("/Home");}}>Home
-        </button>
-
-        <button
-            onClick={() => {navigate("/Chat");}}>Chat
-        </button>
-
-        <button
-            onClick={() => {navigate("/Login");}}>Logout
-        </button>
-      </div>
+        <Navigationbar />
+        <div>
+        </div>
 
         <div id="map">
           <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "50vh", width: "100vh"}}>
@@ -35,6 +21,7 @@ function Maps() {
             <Marker position={[51.505, -0.09]}>
               <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
+                
               </Popup>
             </Marker>
           </MapContainer>
