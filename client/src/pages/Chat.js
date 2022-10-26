a07e6e6bdf6d21e8f0b1d170d9f915a22e42482d
Valentin <valentin@wuerfelmail.de>
2022-10-26 13:30:29 +0200

    added Username function

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,117 +1,119 @@
 import "./css/Chat.css";
 import React, { useEffect, useState, useRef } from "react";
-import { useNavigate } from "react-router-dom";
 import socketIO from "socket.io-client";
-
+import Navigationbar from "./navigationbar";
 const socket = socketIO.connect("http://localhost:4001");
 
+
 function Chat() {
-  const navigate = useNavigate();
+  // Messages Ref to scroll down
+  const messagesRef = useRef(null);
 
-  // AutoScroll to Bottom
-  const fieldRef = useRef(null);
-
+  // States
+  const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
+  const [username, setUsername] = useState("");
+  const [connected, setConnected] = useState(false);
 
-  const sendMessage = () => {
-    socket.emit("send_message", { message });
-  };
-
-  // SocketIO Chat Message Receive and Send to Chat Field
+  // Scroll down to last message
   useEffect(() => {
-    socket.on("receive_message", (data) => {
-      var item = document.createElement("li");
-      item.textContent = data.message;
-      document.getElementById("messages").appendChild(item);
+    messagesRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
 
-      // AutoScroll to Bottom
-      fieldRef.current.scrollIntoView({
-        behavior: "smooth",
-      });
+  // SocketIO
+  useEffect(() => {
+    // SocketIO Disconnect
+    socket.on("disconnect", () => {
+      setConnected(false);
     });
-  }, []);
 
-  // Call Functions when pressing submit
-  function handleSubmit(event) {
-    event.preventDefault();
+    // SocketIO Receive Message and store in MongoDB
+    socket.on("receive_message", (data) => {
+      setMessages([...messages, data]);
+    });
 
-    sendMessage();
+    // SocketIO User Disconnected
+    socket.on("user_disconnected", (id) => {
+      setMessages([...messages, { id: id, message: "disconnected" }]);
+    });
+
+    // SocketIO Set Username
+    socket.on("set_username", (data) => {
+      setUsername(data.username);
+      setConnected(true);
+    });
+
+  }, [messages]);
+
+  // Send Message
+  const sendMessage = (e) => {
+    e.preventDefault();
+    socket.emit("send_message", { username: username, message: message });
     setMessage("");
   }
 
-  socket.on("connect", function () {
-    var client_id = socket.io.engine.id;
-    document.getElementById("SocketID").innerHTML = "ID: " + client_id;
-  });
+  // Set Username and connect to SocketIO with prompt
+  const setUsernameAndConnect = () => {
+    const username = prompt("Username");
+    socket.emit("set_username", { username: username });
+  }
+  
+  if (!connected) {
+    setUsernameAndConnect()
+  }
 
   return (
     <div class="container">
       <div class="row ">
         <div class="navigation col ">
-          <h1> Chat</h1>
-          <button
-            onClick={() => {
-              navigate("/Home");
-            }}
-          >
-            Home
-          </button>
+          <Navigationbar />
+      </div>
 
-          <button
-            onClick={() => {
-              navigate("/Maps");
-            }}
-          >
-            Maps
-          </button>
-
-          <button
-            onClick={() => {
-              navigate("/Login");
-            }}
-          >
-            Logout
-          </button>
-
-          <div class="col">
-            <h3 class="m-3">Socket IDs of all Users</h3>
-            <ul id="users"></ul>
-          </div>
-        </div>
-
-        <div id="chatContainer" class="col">
+      <div id="chatContainer" class="col">
           <div
             id="text"
             class="overflow-auto d-flex flex-column justify-content-between rounded"
           >
-            <ul id="messages"></ul>
-            <div ref={fieldRef}></div>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <div class="row mt-2 mb-2">
-              <div class="col">
-                <input
-                  placeholder="Message..."
-                  required
-                  value={message}
-                  onChange={(event) => {
-                    setMessage(event.target.value);
-                  }}
-                  class="rounded form-control"
-                  type="text"
-                />
-              </div>
-              <div class="col">
-                <button class="btn btn-outline-primary" type="submit">
-                  Send Message
-                </button>
-              </div>
-              <div class="col" id="SocketID"></div>
-            </div>
-          </form>
+            <ul id="messages">
+          {messages.map((message, index) => (
+            <li key={index}>
+              <b>{message.username}</b>: {message.message}
+            </li>
+          ))}
+          </ul>
+          <div ref={messagesRef}></div>
         </div>
+        
+            <div>
+              <form onSubmit={sendMessage}>
+                <div class="row mt-2 mb-2">
+                  <div class="col">
+                    <input
+                      placeholder="Message..."
+                      required
+                      value={message}
+                      onChange={(event) => {
+                        setMessage(event.target.value);
+                      }}
+                      class="rounded form-control"
+                      type="text"
+                    />
+                  </div>
+                  <div class="col">
+                    <button class="btn btn-outline-primary" type="submit">
+                      Send Message
+                    </button>
+                  </div>
+                  <div class="col" id="SocketID"></div>
+                </div>
+              </form>
+            </div>
+          <div className="connection">
+            <p>Connected as: {username}</p>
+          </div>
       </div>
     </div>
+  </div>
   );
 }
 
