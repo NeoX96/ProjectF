1108f8758f5f274368dd5e8444df4fe12205dfdc
Valentin <valentin@wuerfelmail.de>
2022-10-29 21:32:26 +0200

    added ChatBot + seperate Users

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -15,35 +15,45 @@ function Chat() {
   const [username, setUsername] = useState("");
   const [connected, setConnected] = useState(false);
 
-  // Scroll down to last message
-  useEffect(() => {
-    messagesRef.current.scrollIntoView({ behavior: "smooth" });
-  }, [messages]);
+
 
   // SocketIO
   useEffect(() => {
-    // SocketIO Disconnect
-    socket.on("disconnect", () => {
-      setConnected(false);
-    });
+    messagesRef.current.scrollIntoView({ behavior: "smooth" });
 
     // SocketIO Receive Message and store in MongoDB
     socket.on("receive_message", (data) => {
       setMessages([...messages, data]);
     });
 
-    // SocketIO User Disconnected
-    socket.on("user_disconnected", (id) => {
-      setMessages([...messages, { id: id, message: "disconnected" }]);
+    // SocketIO User Disconnected Chat Bot
+    socket.on("user_disconnected", (username) => {
+      setMessages([...messages, { username: "Chat Bot", message: `${username} disconnected` }]);
     });
 
-    // SocketIO Set Username
-    socket.on("set_username", (data) => {
-      setUsername(data.username);
-      setConnected(true);
+    // SocketIO User Connected Chat Bot
+    socket.on("user_connected", (username) => {
+      setMessages([...messages,{ username: "Chat Bot", message: `${username} connected` }]);
     });
 
   }, [messages]);
+
+
+  // const for emit get_messages
+  const getMessagesEmit = () => {
+    socket.emit("get_messages");
+  };
+
+
+  // SocketIO Receive Messages from MongoDB
+  socket.on("get_messages", (data) => {
+    // for each message.username message.message
+    data.forEach((messageIN) => {
+      setMessages([...messages, { username: messageIN.username, message: messageIN.message }]);
+      console.log(`Name: ${messageIN.username} Message: ${messageIN.message}`)
+      });
+    console.log(data);
+  });
 
   // Send Message
   const sendMessage = (e) => {
@@ -55,28 +65,37 @@ function Chat() {
   // Set Username and connect to SocketIO with prompt
   const setUsernameAndConnect = () => {
     const username = prompt("Username");
-    socket.emit("set_username", { username: username });
+    setUsername(username);
+    setConnected(true);
+        // SocketIo Emit Connection to Server
+        socket.emit("user_connected", username);
   }
   
   if (!connected) {
-    setUsernameAndConnect()
+    setUsernameAndConnect();
   }
 
   return (
-    <div class="container">
-      <div class="row ">
-        <div class="navigation col ">
+    <div className="container">
+      <div className="row ">
+        <div className="navigation col ">
           <Navigationbar />
+          <button onClick={getMessagesEmit}>Get Messages</button>
       </div>
 
-      <div id="chatContainer" class="col">
+      <div id="chatContainer" className="col">
           <div
             id="text"
-            class="overflow-auto d-flex flex-column justify-content-between rounded"
+            className="overflow-auto d-flex flex-column justify-content-between rounded"
           >
             <ul id="messages">
           {messages.map((message, index) => (
-            <li key={index}>
+            <li
+              key={index}
+              className={
+                message.username === username ? "text-end" : "text-start"
+              }
+            >
               <b>{message.username}</b>: {message.message}
             </li>
           ))}
@@ -86,25 +105,23 @@ function Chat() {
         
             <div>
               <form onSubmit={sendMessage}>
-                <div class="row mt-2 mb-2">
-                  <div class="col">
+                <div className="row mt-2 mb-2  d-block ">
+                  <div className="col input-group">
                     <input
-                      placeholder="Message..."
+                      placeholder="Message ..."
                       required
                       value={message}
                       onChange={(event) => {
                         setMessage(event.target.value);
                       }}
-                      class="rounded form-control"
+                      className="form-control"
                       type="text"
                     />
-                  </div>
-                  <div class="col">
-                    <button class="btn btn-outline-primary" type="submit">
-                      Send Message
+                    <button className="btn btn-primary" type="submit">
+                      <i className="bi bi-send"></i>
                     </button>
                   </div>
-                  <div class="col" id="SocketID"></div>
+                  <div className="col" id="SocketID"></div>
                 </div>
               </form>
             </div>
