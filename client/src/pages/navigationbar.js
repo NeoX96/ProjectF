a96d085e33c35f1fe993bc28a74e0f41ae5ce99a
Valentin <valentin@wuerfelmail.de>
2022-11-02 13:56:50 +0100

    changed syntax for better reading

--- client/src/pages/navigationbar.js
+++ client/src/pages/navigationbar.js
@@ -1,22 +1,18 @@
+import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 export default function Navigationbar() {
     const navigate = useNavigate();
   
     return (
-    <div>
-      <h1> Maps </h1>
-      <button
-          onClick={() => {navigate("/Home");}}>Home
-      </button>
-  
-      <button
-          onClick={() => {navigate("/Chat");}}>Chat
-      </button>
-  
-      <button
-          onClick={() => {navigate("/Login");}}>Logout
-      </button>
-    </div>
-    )
-  }
+      <div className="navigationbar ml-2">
+        <Button variant="primary" onClick={() => navigate("/Home")}>Home</Button>
+
+        <Button variant="primary" onClick={() => navigate("/Chat")}>Chat</Button>
+
+        <Button variant="primary" onClick={() => navigate("/Maps")}>Maps</Button>
+
+        <Button variant="primary" onClick={() => navigate("/Settings")}>Settings</Button>
+      </div>
+    );
+}
