ce63ec439026084ea791843714d1134614d31444
Rene <Rene_nbg@hotmail.de>
2022-11-06 10:37:30 +0100

    added PWA support

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '@iconify-icon/react';
+import * as serviceWorker from './serviceWorker';
 
 import App from './App';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(<App />);
+
+serviceWorker.register();
