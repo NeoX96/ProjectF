50489489adbc3e97b68379ffbe72aee190ba1f73
Valentin <valentin@wuerfelmail.de>
2022-11-09 14:23:58 +0100

    added current location

--- client/src/pages/Maps.js
+++ client/src/pages/Maps.js
@@ -1,34 +1,66 @@
 import React from "react";
-import './css/Chat.css';
-// Leaflet-React Import
 import { Marker, Popup, TileLayer, MapContainer} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import Navigationbar from "./navigationbar";
+import L from 'leaflet';
+import { useGeolocated } from "react-geolocated";
+import images from './assets/map/index.js';
+
+function GetIcon(_iconSize) {
+  return L.icon({
+    iconUrl: images.me,
+    iconSize: _iconSize
+  });
+}
 
 function Maps() {
-  return (
-    <div>
-        <Navigationbar />
-        <div>
-        </div>
 
-        <div id="map">
-          <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "50vh", width: "100vh"}}>
+
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+        useGeolocated({
+            positionOptions: {
+                enableHighAccuracy: false,
+            },
+            userDecisionTimeout: 5000,
+        });
+
+
+    return !isGeolocationAvailable ? (
+        <div>Your browser does not support Geolocation</div>
+    ) : !isGeolocationEnabled ? (
+        <div>
+            Geolocation is not enabled</div>
+    ) : coords ? (
+      <div>
+
+          
+
+
+        <div id="map" className="vh-100">
+          <MapContainer center={[coords.latitude, coords.longitude]} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%"}}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
+            <Marker position={[coords.latitude, coords.longitude]} icon={GetIcon(40)}>
+              
               <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-                
+                <div>
+                    <h3>Deine Position</h3>
+                    <p>Latitude: {coords.latitude}</p>
+                    <p>Longitude: {coords.longitude}</p>
+                </div>
               </Popup>
+              
             </Marker>
           </MapContainer>
         </div>
 
-    </div>
-  );
+      </div>
+
+
+    ) : (
+        <div>GPS-Daten werden jetzt upgerufen&hellip; </div>
+    );
 }
 
 export default Maps;
