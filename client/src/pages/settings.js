588644158db1ef8d7e3ab69240e05f01a426caae
Valentin <valentin@wuerfelmail.de>
2022-11-09 14:24:49 +0100

    removed navigation

--- client/src/pages/settings.js
+++ client/src/pages/settings.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Navigationbar from "./navigationbar";
 import { useGeolocated } from "react-geolocated";
 
 
@@ -17,7 +16,6 @@ const Demo = () => {
         <div>Your browser does not support Geolocation</div>
     ) : !isGeolocationEnabled ? (
         <div>
-            <Navigationbar />
             Geolocation is not enabled</div>
     ) : coords ? (
         <table>
