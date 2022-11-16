6334d8637228c3dd26aa0c5be4dec8d46aac0592
Valentin <valentin@wuerfelmail.de>
2022-11-16 21:45:40 +0100

    updated Login Page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,21 +1,56 @@
 import React, { useState } from "react"
+import axios from 'axios';
 import { useNavigate } from "react-router-dom";
+
 
 function Login() {
   const navigate = useNavigate();
-  let [authMode, setAuthMode] = useState("signup")
- //const[taskName, setTaskName] = useState( initialState: '');
 
- 
+  const [authMode, setAuthMode] = useState("signin");
+  const [vorname, setVorname] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [geburtstag, setGeburtstag] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const changeAuthMode = () => {
     setAuthMode(authMode === "signin" ? "signup" : "signin")
   }
 
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const registered = {
+        vorname: vorname,
+        nickname: nickname,
+        birthday: geburtstag,
+        email: email,
+        password: password
+    };
+
+    axios.post('http://localhost:4000/createUser', registered).then(res => {
+        console.log(res.data);
+        navigate("/Home");
+    }).catch(err => {
+        console.log(err);
+    });
+
+    console.log(registered);
+  }
+
+
+        
+
   if (authMode === "signin") {
     return (
       <div className="justify-content-center d-flex">
       <div className="Auth-form-container w-25">
+      <div className="text-center">
+          <p>Not registered?</p>
+          <span className="link-primary" onClick={changeAuthMode}>
+            Sign UP
+          </span>
+        </div>
         <form className="Auth-form">
           <div className="Auth-form-content">
             <h6 >Welcome to SPORTSCONNECT</h6>
@@ -53,42 +88,56 @@ function Login() {
 
   return (
     <div className="justify-content-center d-flex">
-    <div className="Auth-form-container w-25">
-      <form className="Auth-form">
+      <div className="Auth-form-container w-25">
+        <div className="text-center">
+          <p>Already registered?</p>
+          <span className="link-primary" onClick={changeAuthMode}>
+            Sign In
+          </span>
+        </div>
+
+        <form className="Auth-form" onSubmit={handleRegister}>
+        <h3 className="Auth-form-title">Registrieren</h3>
         <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign In</h3>
-          <div className="text-center">
-            Already registered?{" "}
-            <span className="link-primary" onClick={changeAuthMode}>
-              Sign In
-            </span>
-          </div>
-          <div className="form-group mt-3">
-            <label>Full Name</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="e.g Jane Doe"
-            />
-          </div>
           <div className="form-group mt-3">
             <label>Email address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Email Address"
+            <input type="email" className="form-control mt-1" placeholder="Email Addresse" required
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
             />
+          </div>
+          <div className="form-group mt-3">
+            <label>Vorname</label>
+            <input type="text" className="form-control mt-1" placeholder="Vorname" required
+              onChange={(event) => {
+                setVorname(event.target.value);
+              }}/>
+          </div>
+          <div className="form-group mt-3">
+            <label>Benutzername</label>
+            <input type="text" className="form-control mt-1" placeholder="Nickname" required
+              onChange={(event) => { 
+                setNickname(event.target.value);
+              }}/>
+          </div>
+          <div className="form-group mt-3">
+            <label>Geburtstag</label>
+            <input type="date" className="form-control mt-1" placeholder="Geburstag" required
+              onChange={(event) => {
+                setGeburtstag(event.target.value);
+              }}/>
           </div>
           <div className="form-group mt-3">
             <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Password"
-            />
+            <input type="password" className="form-control mt-1" placeholder="Passwort"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+              />
           </div>
           <div className="d-grid gap-2 mt-3">
-            <button onClick={() => {navigate("/Home")}} className="btn btn-primary">
+            <button type="submit" className="btn btn-primary">
               Submit
             </button>
           </div>
