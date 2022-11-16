25262024a29ca57c7f20dc1c2bc2f08b45775737
Valentin <valentin@wuerfelmail.de>
2022-11-16 10:33:44 +0100

    removed button

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Suspense, lazy} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Navbar from './components/navigationbar';
-import { Button } from "react-bootstrap";
 
 
 const Home = lazy(() => import('./pages/Home'));
