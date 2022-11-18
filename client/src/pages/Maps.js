fe709cf7c24b4a9cf2d7ed76f482e4352944bc6b
Lisa <Lisa.Hirn@gmx.netz>
2022-11-18 09:51:32 +0100

    Standort/Umkreis

--- client/src/pages/Maps.js
+++ client/src/pages/Maps.js
@@ -1,10 +1,17 @@
-import React from "react";
-import { Marker, Popup, TileLayer, MapContainer} from 'react-leaflet';
+import React, {useState, useEffect} from "react";
+import {MapContainer, TileLayer, Marker, Popup, useMap,Circle } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { useGeolocated } from "react-geolocated";
 import images from './assets/map/index.js';
+import 'leaflet/dist/leaflet.css';
 
+
+
+
+
+
+
+ 
 function GetIcon(_iconSize) {
   return L.icon({
     iconUrl: images.me,
@@ -12,55 +19,79 @@ function GetIcon(_iconSize) {
   });
 }
 
+
 function Maps() {
 
+  
+   
 
-  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
-        useGeolocated({
-            positionOptions: {
-                enableHighAccuracy: false,
-            },
-            userDecisionTimeout: 5000,
-        });
-
-
-    return !isGeolocationAvailable ? (
-        <div>Your browser does not support Geolocation</div>
-    ) : !isGeolocationEnabled ? (
-        <div>
-            Geolocation is not enabled</div>
-    ) : coords ? (
-      <div>
-
-          
-
-
-        <div id="map" className="vh-100">
-          <MapContainer center={[coords.latitude, coords.longitude]} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%"}}>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[coords.latitude, coords.longitude]} icon={GetIcon(40)}>
-              
-              <Popup>
-                <div>
-                    <h3>Deine Position</h3>
-                    <p>Latitude: {coords.latitude}</p>
-                    <p>Longitude: {coords.longitude}</p>
-                </div>
-              </Popup>
-              
-            </Marker>
-          </MapContainer>
-        </div>
-
-      </div>
-
-
-    ) : (
-        <div>GPS-Daten werden jetzt upgerufen&hellip; </div>
+ 
+ 
+  const [zoom, setZoom] = useState(15);
+ 
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const map = useMap();
+  
+    useEffect(() => {
+      map.locate().on("locationfound", function (e) {
+        setPosition(e.latlng);
+        setZoom(13);
+        map.setView(e.latlng, map.getZoom());
+      });
+    }, [map]);
+  
+    return position === null ? null : (
+      <Circle center={position} radius={100} fillColor={'green'} color={'black'}>
+        <Marker position={position} icon={GetIcon(40)}>
+        <Popup>
+          Dein Standort
+        </Popup>
+        
+      </Marker>
+      </Circle>
+      
     );
-}
+  }
+ 
+ 
+  return (
 
+    
+    <MapContainer 
+    
+
+    
+      center={[48.777500, 11.431111]}
+      zoom={zoom}
+      scrollWheelZoom
+      style={{ height: "100vh" }}
+    >
+  <body background color="green">
+
+<nav >
+   <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Über uns</a></li>
+      <li><a href="#">Leistungen</a></li>
+      <li><a href="#">FAQ</a></li>
+      <li><a href="#">Kontakt</a></li>
+  </ul>
+</nav>
+
+</body>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <LocationMarker>
+         
+        </LocationMarker>
+
+   
+    </MapContainer>
+  );
+}
+ 
+ 
 export default Maps;
