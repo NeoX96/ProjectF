20ba52fb8706de87912bd9ee50065e08a104d867
Valentin <valentin@wuerfelmail.de>
2023-02-13 14:55:15 +0100

    added login route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const UserModel = require('../models/Users');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 dotenv.config();
+const JWTSEC = process.env.JWTSEC;
+
+const UserModel = require('../models/Users');
 const crypto = require('crypto');
 const randomId = () => crypto.randomBytes(16).toString("hex");
 
@@ -20,6 +25,40 @@ router.post("/createUser", async (req, res) => {
     res.json(user);
 });
 
+router.post("/login", async (req, res) => {
+    console.log(req.body);
+    console.log("login");
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return res.status(400).json({ error: "Some error occurred" });
+    }
+
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(400).json({ error: "User not found" });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({ error: "Password error" });
+        }
+
+        const accessToken = jwt.sign({
+            id: user._id,
+            username: user.username
+        }, JWTSEC);
+
+        if (accessToken) {
+            user.sessionID = accessToken;
+            await user.save();
+        }
+
+        res.status(200).json({ accessToken: `${accessToken}` });
+    } catch (error) {
+        res.status(500).json({ error: "Internal error occurred" });
+    }
+});
 
 
 
