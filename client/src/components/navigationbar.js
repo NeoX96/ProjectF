51b762f8011c813ec153259e902c401d650483c3
Valentin <valentin@wuerfelmail.de>
2023-02-14 21:24:53 +0100

    structure of code

--- client/src/components/navigationbar.js
+++ client/src/components/navigationbar.js
@@ -7,52 +7,26 @@ import { useNavigate } from "react-router-dom";
 
 
 export default function Navigationbar() {
-    const navigate = useNavigate();
-    // const user = localStorage.getItem("sessionID");
+  const navigate = useNavigate();
 
-    /* const logout = () => {
-      localStorage.removeItem("sessionID");
-        navigate("/");
-    }; */
+  const navLinks = [
+    { name: 'Home', path: '/Home', icon: faHome },
+    { name: 'Map', path: '/Maps', icon: faMap },
+    { name: 'Chat', path: '/Chat', icon: faComment }
+  ];
 
-    // only return if user is on home, chat or maps page
-    if (window.location.pathname === "/Home" || window.location.pathname === "/Chat" || window.location.pathname === "/Maps" || window.location.pathname === "/settings") {
-
-    return (
-      <div>
-
-        <Nav  className="navbar">
-          <NavItem className="nav-item navlink">
-            <Nav.Link onClick={() => navigate("/Home")} className="nav-link active">
-              <div className="icon-container">
-                <FontAwesomeIcon className="icon" icon={faHome} />
-              </div>
-              Home
-            </Nav.Link>
-          </NavItem>
-          <NavItem className="nav-item navlink">
-            <Nav.Link onClick={() => navigate("/Maps")} className="nav-link active">
+  return (
+    <Nav className="navbar">
+      {navLinks.map((link) => (
+        <NavItem key={link.path} className="nav-item navlink">
+          <Nav.Link onClick={() => navigate(link.path)} className="nav-link active">
             <div className="icon-container">
-              <FontAwesomeIcon className="icon" icon={faMap} />
+              <FontAwesomeIcon className="icon" icon={link.icon} />
             </div>
-              Map
-            </Nav.Link>
-          </NavItem>
-          <NavItem className="nav-item navlink">
-            <Nav.Link onClick={() => navigate("/Chat")} className="nav-link active">
-            <div className="icon-container">
-            <FontAwesomeIcon className="icon" icon={faComment} />
-              </div>
-              Chat
-            </Nav.Link>
-          </NavItem>
-        </Nav>
-      </div>
-    );
-    } else {
-        return (
-            <div></div>
-        );
-    }
-
+            {link.name}
+          </Nav.Link>
+        </NavItem>
+      ))}
+    </Nav>
+  );
 }
