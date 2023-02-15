8883c657c0e73872797d9b59699d8fcbb9df12b7
Valentin <valentin@wuerfelmail.de>
2023-02-15 01:06:37 +0100

    changed bootstrap things

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,34 +1,36 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from 'react';
-import { Button, Form  } from 'react-bootstrap';
-import Cookies from 'js-cookie';
-import axios from 'axios';
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import Cookies from "js-cookie";
+import axios from "axios";
 
-
-function Login () {
+function Login() {
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const isDisabled =
+    email.trim() === "" || password.trim() === "" || !email.includes("@");
 
   const changeAuthMode = () => {
     navigate("/Register");
-  }
+  };
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
-      const { data } = await axios.post('http://localhost:4000/login', {
+      const { data } = await axios.post("http://localhost:4000/login", {
         email,
-        password
+        password,
       });
 
       if (data.accessToken) {
-        Cookies.set('sessionID', data.accessToken, {
+        Cookies.set("sessionID", data.accessToken, {
           expires: 1,
           sameSite: "none",
-          secure: true
+          secure: true,
         });
 
         window.location.href = "/Home";
@@ -38,34 +40,53 @@ function Login () {
     } catch (error) {
       console.log(error.response);
       alert("Error logging in. Please try again.");
-      Cookies.remove('sessionID');
+      Cookies.remove("sessionID");
     }
   };
 
   return (
-    <div className="d-flex justify-content-center">
-        <div className="">
-            <div className="text-center">
-                <h6>Welcome to SPORTSCONNECT</h6>
-                <Button onClick={changeAuthMode}>
-                    Sign Up
-                </Button>
-            </div>
-            <div className="">
-                <h3>Login</h3>
-                <div className="form-group mt-3">
-                    <Form.Control className="mt-1" type="email" name="" id="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div className="form-group mt-3">
-                    <Form.Control className="mt-1" type="password" placeholder='Password' name="" onChange={(e) => setPassword(e.target.value)} id="password" />
-                </div>
-                <div className="d-flex justify-content-center mt-3">
-                    <Button onClick={handleLogin}>Submit</Button>
-                </div>
-            </div>
+    <div className="d-flex justify-content-center text-center">
+      <div className="">
+        <div>
+          <h6>Welcome to SPORTSCONNECT</h6>
+          <Button onClick={changeAuthMode}>Sign Up</Button>
         </div>
+        <Form onSubmit={handleLogin}>
+          <div className="">
+            <div className="mt-3">
+              <Form.Control
+                className="mt-1"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mt-3">
+              <Form.Control
+                className="mt-1"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                id="password"
+              />
+            </div>
+            <div className="d-flex justify-content-center mt-3">
+              <Button
+                variant={isDisabled ? "secondary" : "primary"}
+                type="submit"
+                disabled={isDisabled}
+              >
+                Login
+              </Button>
+            </div>
+          </div>
+        </Form>
+        <div className="mt-5">test@rdf.de - asdfasdf</div>
+      </div>
     </div>
-);
+  );
 }
 
-export default Login
+export default Login;
