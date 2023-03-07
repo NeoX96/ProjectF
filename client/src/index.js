d24adfda1bdbf7acee70ef62cce7f37704d55fed
Valentin <valentin@wuerfelmail.de>
2023-03-07 02:24:34 +0100

    exported variables

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,11 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '@iconify-icon/react';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-
 import App from './App';
+
+export const DOMAIN = "http://localhost:4000";
+export const endpoint =  "http://localhost:4001"
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
