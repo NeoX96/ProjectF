9a84b2adb510daaaa552baf25b0067ddaa508fbc
Valentin <valentin@wuerfelmail.de>
2023-03-07 22:11:33 +0100

    changed variables

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,8 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-export const DOMAIN = "http://localhost:4000";
-export const endpoint =  "http://localhost:4001"
+export const DOMAIN = "http://localhost:4000/api";
+export const endpoint =  "http://localhost:4001/socket";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
