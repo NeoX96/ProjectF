74c7a77f37957f208055a0a7aa6a5473aa8b71be
Valentin <valentin@wuerfelmail.de>
2023-03-09 01:54:25 +0100

    react-router-dom naviagte

--- client/src/pages/Verify.js
+++ client/src/pages/Verify.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useNavigate } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import OtpInput from "react18-input-otp";
 import axios from "axios";
