5598c383f2c5f2c1060e7ef14ae7183b09a11d08
Valentin <valentin@wuerfelmail.de>
2023-03-09 01:41:19 +0100

    updating domain

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,8 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-var isGonkle = window.location.hostname === "gonkle.de" ? true : false;
+
+var isGonkle = window.location.hostname === "gonkle.de" || window.location.hostname === "www.gonkle.de" ? true : false;
 
 export const DOMAIN = isGonkle ? "https://gonkle.de/api" : "http://localhost:4000/api";
 export const endpoint = isGonkle ? "https://gonkle.de/socket" : "http://localhost:4001/socket";
