d3ecb8659172d440849e995947521f2842cc0281
Valentin <valentin@wuerfelmail.de>
2023-03-10 22:37:47 +0100

    changed style

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,103 +1,74 @@
+import { Box, Button, Container } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import images from "./assets/home";
 import { useNavigate } from "react-router-dom";
-import { useState, } from "react";
-import { Button, Form } from "react-bootstrap";
-import Cookies from "js-cookie";
-import axios from "axios";
-import images from './assets/home/index';
+import Title from "../components/title";
 
 
-function Login() {
-    const navigate = useNavigate();
+const MainContainer = styled(Container)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  minHeight: "100vh",
+  overflow: "hidden",
+});
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+const StyledBox = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "column",
+  gap: "20px",
+  padding: "15px",
+  paddingBottom: "30px",
+  borderRadius: "25px",
+  backgroundColor: "rgba(0, 10, 30, 0.9)", // setzen Sie eine Hintergrundfarbe für die Box
+  boxShadow: "8px 50px 25px rgba(0, 0, 0, 0.6)", // fügen Sie einen Schatten zur Box hinzu
+});
 
-    const isDisabled =
-        email.trim() === "" || password.trim() === "" || !email.includes("@");
+const StyledImg = styled("img")({
+  width: "auto",
+  borderRadius: "10px",
+  height: "50vh",
+});
 
-    const changeAuthMode = () => {
-        navigate("/Register");
-    };
+const LandingPage = () => {
+  const navigate = useNavigate();
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
+  return (
+    <MainContainer>
+      <Box>
+        <Box className="d-flex">
+          <Title />
+        </Box>
+        <StyledBox sx={{ mb: 5 }}>
+          <StyledImg src={images.sportsgif} alt="my" />
+          <Box sx={{ display: "flex", gap: "40px" }}>
+            <Button
+              variant="contained"
+              size="large"
+              onClick={() => navigate("/Register")}
+              sx={{
+                borderRadius: "50px",
+              }}
+            >
+              Sign Up
+            </Button>
+            <Button
+              variant="contained"
+              size="large"
+              onClick={() => navigate("/Login")}
+              sx={{
+                borderRadius: "50px",
+              }}
+            >
+              Login
+            </Button>
+          </Box>
+        </StyledBox>
+      </Box>
+    </MainContainer>
+  );
+};
 
-        try {
-            const { data } = await axios.post("http://localhost:4000/login", {
-                email,
-                password,
-            });
-
-            if (data.accessToken) {
-                Cookies.set("sessionID", data.accessToken, {
-                    expires: 1,
-                    sameSite: "none",
-                    secure: true,
-                });
-
-                window.location.href = "/Home";
-            } else {
-                alert("Access token is not returned");
-            }
-        } catch (error) {
-            console.log(error.response);
-            alert("Error logging in. Please try again.");
-            Cookies.remove("sessionID");
-        }
-    };
-
-    return (
-
-        
-
-        <div className="d-flex justify-content-center mt-5">
-            <div className="">
-                
-              
-                <header class="bg-primary-subtle d-flex justify-content-center mt-5 rounded">
-                    <Form className="w-100" onSubmit={handleLogin}>
-                        <div className="d-flex justify-content-center align-items-center">
-                            <div>
-                                <Form.Control
-                                    className="mt-1"
-                                    type="email"
-                                    placeholder="Email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </div>
-                            <div class="mx-3">
-                                <Form.Control
-                                    className="mt-1"
-                                    type="password"
-                                    placeholder="Password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    id="password"
-                                />
-                            </div>
-                            <div class="d-flex justify-content-center align-items-center border-box">
-                                <Button className="btn btn-primary mx-3 h-50"
-                                    variant={isDisabled ? "secondary" : "primary"}
-                                    type="submit"
-                                    disabled={isDisabled}
-                                >
-                                    Login
-                                </Button>
-                                <Button className="btn btn-primary h-25" onClick={changeAuthMode}>SignUp</Button>
-                            </div>
-                           
-                        </div>
-                    </Form>
-                </header>
-                <div class="d-flex flex-column align-items-center">
-                    <img class="w-50 mt-5 rounded" src={images.sportsgif} alt="erklärung"/>
-                    <img class="w-50 mt-5 rounded" src={images.okbutton} alt="gonklelogo"/>
-                </div>
-            </div>
-   
-        </div>
-    );
-}
-
-export default Login;
+export default LandingPage;
