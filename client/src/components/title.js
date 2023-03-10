bc626f871e858fa3ea65703855a298d85c08ecbf
Valentin <valentin@wuerfelmail.de>
2023-03-10 23:14:14 +0100

    added gonkle logo

--- client/src/components/title.js
+++ client/src/components/title.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Box, Container, Typography } from "@mui/material";
 
 const Title = () => {
+  const navigate = useNavigate();
+
   return (
-    <Container sx={{ display: "flex", justifyContent: "center"}}>
+    <Container sx={{ display: "flex", justifyContent: "center", mb: 1}} >
       <Box>
         <Typography
+          onClick={() => navigate("/Home")}
           sx={{
             fontSize: "4rem",
             color: "red",
             fontWeight: "bold",
             fontFamily: "Montserrat, sans-serif",
-            textShadow: "4px 4px 4px rgba(100, 0, 0, 0.99)",
+            textShadow: "4px 4px 4px rgba(100, 0, 0, 0.95)",
+            textDecoration: "none", cursor: "pointer",
           }}
         >
           Go
@@ -19,11 +24,13 @@ const Title = () => {
       </Box>
       <Box>
         <Typography
+          onClick={() => navigate("/Home")}
           sx={{
             fontSize: "4rem",
             fontWeight: "bold",
             fontFamily: "Montserrat, sans-serif",
             textShadow: "4px 4px 4px rgba(30, 30, 30, 0.6)",
+            textDecoration: "none", cursor: "pointer",
           }}
         >
           nkle
