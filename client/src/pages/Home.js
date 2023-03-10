e68d67f61e33709104eda53240a5443ec02c9ffa
Valentin <valentin@wuerfelmail.de>
2023-03-10 03:50:53 +0100

    implenented MUI styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,74 +1,154 @@
-import React, {  useContext } from "react";
-import images from './assets/home/index';
+import React, { useContext } from "react";
+import images from "./assets/home/index";
 import { useNavigate } from "react-router-dom";
-import { Carousel, Button } from 'react-bootstrap';
+import { Carousel } from "react-bootstrap";
 import "./css/Home.css";
-import {IndexContext} from "../App";
-// import box from mui
-import { Box } from '@mui/material';
+import { IndexContext } from "../App";
+import Cookies from "js-cookie";
+import { Box, Container, Fab, Grid } from "@mui/material";
+import NavigationIcon from "@mui/icons-material/Navigation";
 
 function Home() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const {index, setIndex} = useContext(IndexContext);
+  const { index, setIndex } = useContext(IndexContext);
+  const vorname = Cookies.get("vorname");
 
-    const handleSelect = (selectedIndex, e) => {
-      setIndex(selectedIndex);
-    };
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
-    console.log("index: " + index);
+  console.log("index: " + index);
 
-    return (
-        <div className="justify-content-center container home">
-          <div className="Überschrift justify-content-center d-flex m-5">
-            <h1>WELCOME TO Gonkle</h1>
-          </div>
+  return (
+    <div
+      style={{
+        height: "calc(100vh - 150px)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "30px",
+      }}
+    >
+      <Container>
+        <Box
+          sx={{
+            backdropFilter: "blur(5px)",
+            backgroundColor: "rgba(255, 255, 255, 0.7)",
+            borderRadius: 5,
+            p: 2,
+            boxShadow: 20,
+          }}
+        >
+          <Grid container justifyContent="center" mb={3}>
+            <Grid item>
+              <Box
+                sx={{
+                  p: 2,
+                  border: "1px dashed grey",
+                  borderRadius: 5,
+                  backgroundColor: "rgba(0, 255, 255, 0.15)",
+                  boxShadow: 3,
+                }}
+              >
+                <h1>Hey, {vorname}</h1>
+              </Box>
+            </Grid>
+          </Grid>
 
-          <div className="justify-content-center d-flex">
-            <h6>Wähle deine Sportart aus</h6>
-          </div>
+          <Grid container justifyContent="center" className="mb-3">
+            <Grid item>
+              <h6>Wähle deine Sportart aus</h6>
+            </Grid>
+          </Grid>
 
-          <div className="justify-content-center d-flex align-items-center">
-            <Box  >
-            <Carousel  interval={null} touch={true} keyboard={true} activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                  <img src={images.frisbee} alt="Frisbee" className="photo"/>
-                </Carousel.Item>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            className="mb-3"
+          >
+            <Grid item>
+              <Box
+                sx={{
+                  borderRadius: 5,
+                  backgroundColor: "rgba(0, 255, 255, 0.1)",
+                  boxShadow: 3,
+                }}
+              >
+                <Carousel
+                  interval={null}
+                  touch={true}
+                  keyboard={true}
+                  activeIndex={index}
+                  onSelect={handleSelect}
+                  style={{ width: "300px" }}
+                >
+                  <Carousel.Item>
+                    <img src={images.frisbee} alt="Frisbee" className="photo" />
+                  </Carousel.Item>
 
-                <Carousel.Item>
-                  <img src={images.fussball} alt="Fußball" className="photo"/>
-                 
-                </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      src={images.fussball}
+                      alt="Fußball"
+                      className="photo"
+                    />
+                  </Carousel.Item>
 
-                <Carousel.Item>
-                  <img src={images.volleyball} alt="Volleyball" className="photo"/>
-                
-                </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      src={images.volleyball}
+                      alt="Volleyball"
+                      className="photo"
+                    />
+                  </Carousel.Item>
 
-                <Carousel.Item>
-                  <img src={images.baskettball} alt="Baskettball" className="photo"/>
-                 
-                </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      src={images.baskettball}
+                      alt="Baskettball"
+                      className="photo"
+                    />
+                  </Carousel.Item>
 
-                <Carousel.Item>
-                  <img src={images.tischtennis} alt="Tischtennis" className="photo"/>
-                 
-                </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      src={images.tischtennis}
+                      alt="Tischtennis"
+                      className="photo"
+                    />
+                  </Carousel.Item>
 
-                <Carousel.Item>
-                  <img src={images.skateboard} alt="Skateboard" className="photo"/>
-                 
-                </Carousel.Item>
-            </Carousel>
-            </Box>
-          </div>
-            
-            <div className="justify-content-center d-flex mt-5">
-              <Button  onClick={() => {navigate("/Maps")}} className="btn btn-secondary justify-content-center d-flex" >LET´S PLAY !</Button>
-            </div>
-        </div>
-      
-    );
+                  <Carousel.Item>
+                    <img
+                      src={images.skateboard}
+                      alt="Skateboard"
+                      className="photo"
+                    />
+                  </Carousel.Item>
+                </Carousel>
+              </Box>
+            </Grid>
+          </Grid>
+
+          <Grid container justifyContent="center" mt={4}>
+            <Grid item>
+              <Fab
+                color="primary"
+                variant="extended"
+                aria-label="Go"
+                onClick={() => navigate("/Maps")}
+              >
+                <NavigationIcon sx={{ mr: 1 }} />
+                Gonkle
+              </Fab>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </div>
+  );
 }
 
 export default Home;
