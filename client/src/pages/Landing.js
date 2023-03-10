9709693ff3d15fb9faa77e65eba5a39ae57f6269
docnopain <schmid.xyz@gmail.com>
2023-03-10 09:25:55 +0100

    Merge branch 'master'

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, View, Image } from "react";
 import { Button, Form } from "react-bootstrap";
 import Cookies from "js-cookie";
 import axios from "axios";
 import images from './assets/home/index';
+import Navbar from '../components/navigationbar';
+//öimport "./css/Landing.css";
 
 function Login() {
     const navigate = useNavigate();
@@ -46,73 +48,52 @@ function Login() {
     };
 
     return (
-        <div className="d-flex justify-content-center text-center">
+
+
+        <div className="d-flex justify-content-center mt-5">
             <div className="">
-                <div>
-                    <h6>Welcome to Gonkle</h6>
-
+              
+                <header class="bg-primary-subtle d-flex justify-content-center mt-5 rounded">
+                    <Form className="w-75" onSubmit={handleLogin}>
+                        <div className="d-flex justify-content-center align-items-center">
+                            <div>
+                                <Form.Control
+                                    className="mt-1"
+                                    type="email"
+                                    placeholder="Email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </div>
+                            <div class="mx-3">
+                                <Form.Control
+                                    className="mt-1"
+                                    type="password"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    id="password"
+                                />
+                            </div>
+                            <div class="d-flex justify-content-center align-items-center border-box">
+                                <Button className="btn btn-primary mx-3 h-25"
+                                    variant={isDisabled ? "secondary" : "primary"}
+                                    type="submit"
+                                    disabled={isDisabled}
+                                >
+                                    Login
+                                </Button>
+                                <Button className="btn btn-primary h-25" onClick={changeAuthMode}>Signup</Button>
+                            </div>
+                        </div>
+                    </Form>
+                </header>
+                <div class="d-flex flex-column align-items-center">
+                    <img class="w-75 mt-5 rounded" src={images.gonkle} alt="erklärung"/>
+                    <img class="w-25 mt-5 rounded" src={images.frisbee} alt="sportart"/>
                 </div>
-                <Form onSubmit={handleLogin}>
-                    <div className="">
-                        <div className="mt-3">
-                            <Form.Control
-                                className="mt-1"
-                                type="email"
-                                placeholder="Email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                        <div className="mt-3">
-                            <Form.Control
-                                className="mt-1"
-                                type="password"
-                                placeholder="Password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                id="password"
-                            />
-                        </div>
-                        <div className="d-flex justify-content-center mt-3">
-                            <Button
-                                variant={isDisabled ? "secondary" : "primary"}
-                                type="submit"
-                                disabled={isDisabled}
-                            >
-                                Login
-                            </Button>
-                            <Button onClick={changeAuthMode}>Sign Up</Button>
-                        </div>
-                    </div>
-                </Form>
-                <hr></hr>
-                <img src={images.gonkle2} className="photo" alt=""/>
-
-
-
-                <div className="mt-5">Was ist Gonkle ?</div>
-                <div className="mt-5">Gonkle ist eine Social-Media-Platform für Freizeit-Sportler</div><hr></hr>
-                <div className="mt-5">... eine kleine Anleitung ...</div>
-                <hr></hr>
-                <div>
-                    <div><img src={images.intro1} alt="intro1" className="pic" /></div><div><h1>Wähle deine Lieblingssportart</h1>
-                        Wähle aus verschiedenen Sportarten deine Favoriten – hier ist für jeden etwas dabei.
-                    </div></div><hr></hr>
-                <div><img src={images.intro2} alt="intro1" className="pic" /></div><div><h1>Finde Mitspieler</h1>
-                    Plane Deine Partien im Voraus und lade deine Freunde dazu ein. Wähle den Ort, die Uhrzeit und die Sportart.
-                </div><hr></hr>
-                <div><img src={images.intro3} alt="intro1" className="pic" /></div><div><h1>
-
-                    Tritt Partien bei</h1>
-                    Auf der Karte werden Dir Spiele in deiner Nähe angezeigt. Tritt ihnen bei und lerne neue Leute kennen!
-
-                </div><hr></hr>
-                <div><img src={images.intro4} alt="intro1" className="pic" /></div><div><h1>Bleib im Kontakt</h1>
-                    Einzel- und Gruppenchats ermöglichen den entspannten Austausch zwischen Dir und Deinen Mitspielern.
-
-                </div><hr></hr>
-                <div className="mt-5">Dates were yesterday. Today you Gonkle</div>
             </div>
+            <Navbar />
         </div>
     );
 }
