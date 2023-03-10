1fe8322ee0c89961cc13a3178a4e11a26e407793
Valentin <valentin@wuerfelmail.de>
2023-03-10 01:35:18 +0100

    styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React, {  useContext } from "react";
 import images from './assets/home/index';
 import { useNavigate } from "react-router-dom";
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Button } from 'react-bootstrap';
 import "./css/Home.css";
 import {IndexContext} from "../App";
+// import box from mui
+import { Box } from '@mui/material';
 
 function Home() {
     const navigate = useNavigate();
@@ -17,47 +19,52 @@ function Home() {
     console.log("index: " + index);
 
     return (
-        <div >
+        <div className="justify-content-center container home">
+          <div className="Überschrift justify-content-center d-flex m-5">
+            <h1>WELCOME TO Gonkle</h1>
+          </div>
+
           <div className="justify-content-center d-flex">
             <h6>Wähle deine Sportart aus</h6>
           </div>
 
-          <div className="justify-content-center d-flex">
-            <Carousel wrap={true} touch={true} keyboard={true} activeIndex={index} onSelect={handleSelect}>
+          <div className="justify-content-center d-flex align-items-center">
+            <Box  >
+            <Carousel  interval={null} touch={true} keyboard={true} activeIndex={index} onSelect={handleSelect}>
                 <Carousel.Item>
                   <img src={images.frisbee} alt="Frisbee" className="photo"/>
-                  <Carousel.Caption><h6>NR 1 Frisbee</h6></Carousel.Caption>
                 </Carousel.Item>
 
                 <Carousel.Item>
                   <img src={images.fussball} alt="Fußball" className="photo"/>
-                  <Carousel.Caption><h6>NR 2 Fußball</h6></Carousel.Caption>
+                 
                 </Carousel.Item>
 
                 <Carousel.Item>
                   <img src={images.volleyball} alt="Volleyball" className="photo"/>
-                  <Carousel.Caption><h6>NR 3 Volleyball</h6></Carousel.Caption>
+                
                 </Carousel.Item>
 
                 <Carousel.Item>
                   <img src={images.baskettball} alt="Baskettball" className="photo"/>
-                  <Carousel.Caption><h6>NR 4 Baskettball</h6></Carousel.Caption>
+                 
                 </Carousel.Item>
 
                 <Carousel.Item>
                   <img src={images.tischtennis} alt="Tischtennis" className="photo"/>
-                  <Carousel.Caption><h6>NR 5 Tischtennis</h6></Carousel.Caption>
+                 
                 </Carousel.Item>
 
                 <Carousel.Item>
                   <img src={images.skateboard} alt="Skateboard" className="photo"/>
-                  <Carousel.Caption><h6>NR 6 Skateboard</h6></Carousel.Caption>
+                 
                 </Carousel.Item>
             </Carousel>
+            </Box>
           </div>
             
             <div className="justify-content-center d-flex mt-5">
-              <button  onClick={() => {navigate("/Maps")}} className="btn btn-secondary justify-content-center d-flex" >LET´S PLAY !</button>
+              <Button  onClick={() => {navigate("/Maps")}} className="btn btn-secondary justify-content-center d-flex" >LET´S PLAY !</Button>
             </div>
         </div>
       
